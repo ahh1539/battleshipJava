8ca166c5a453ea144b8f2f7e1138b1947b4325bd
James Heliotis <jimelihel@icloud.com>
2018-02-27 18:42:08 -0500

    Update BattleshipException.java
    Added TODOs

--- src/battleship/BattleshipException.java
+++ src/battleship/BattleshipException.java
@@ -4,17 +4,17 @@ public class BattleshipException extends Exception {
 
     public static final int UNSET = -1;
 
-    // Create public integer fields row and column.
+    // TODO Create public integer fields row and column.
     // Make them so they cannot be changed, and non-static.
 
-    // Complete this constructor so that the row and column
+    // TODO Complete this constructor so that the row and column
     // are stored in the exception instance.
     //
     public BattleshipException( int row, int column, String message ) {
         super( message + ", row=" + row + ", column=" + column );
     }
 
-    // Add a second (overloading) constructor that only takes a
+    // TODO Add a second (overloading) constructor that only takes a
     // message string. It should pass the string up to its superclass
     // and set row and column to UNSET.
 }
