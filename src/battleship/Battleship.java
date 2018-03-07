873aea563ce573d034a83634f44faf7bdbc6fa31
ahh1539 <31779206+ahh1539@users.noreply.github.com>
2018-03-06 21:07:17 -0500

    working on main helper functions

--- src/battleship/Battleship.java
+++ src/battleship/Battleship.java
@@ -1,5 +1,8 @@
 package battleship;
 
+import java.io.*;
+import java.util.ArrayList;
+
 public class Battleship {
 
     public static final String ALL_SHIPS_SUNK = "All ships sunk!";
@@ -18,7 +21,107 @@ public class Battleship {
 
     public static final String WHITESPACE = "\\s+";
 
+    private Board board;
+
+    private ArrayList<Ship> ships = new ArrayList<Ship>();
+
+
+
+    public Battleship(String[] args) throws OverlapException, OutOfBoundsException {
+        try{
+            FileInputStream read = new FileInputStream(args[0]);
+            ObjectInputStream obj = new ObjectInputStream(read);
+            Battleship battle = (Battleship) obj.readObject();
+            battle.play();
+        }
+        catch (FileNotFoundException thing){
+            System.out.println(MISSING_SETUP_FILE);
+        }
+        catch (IOException | ClassNotFoundException thingy){
+            setUp(args);
+        }
+    }
+
+
     public static void main(String[] args){
 
     }
+
+    private boolean save(String[]args){
+        try {
+            OutputStream out = new FileOutputStream(args[0]);
+            ObjectOutputStream oon = new ObjectOutputStream(out);
+            oon.writeObject(this);
+            return true;
+        }
+        catch (FileNotFoundException thing){
+            System.out.println(MISSING_SETUP_FILE);
+            return false;
+        }
+        catch (IOException thingy){
+            System.out.println("arigato gozaimas sempai!!!");
+            return false;
+        }
+
+    }
+
+    /*
+    this is a helper function that sets up the arraylist of
+     */
+    private void setUp(String[]args) throws OverlapException, OutOfBoundsException {
+        try {
+            FileReader file = new FileReader(args[0]);
+            BufferedReader read = new BufferedReader(file);
+            String line;
+            int count = 0;
+            while (!((line = read.readLine()).equals(""))){
+                String[] ary = line.split(" ");
+                if(count == 0){
+                    board = new Board(Integer.parseInt(ary[0]),Integer.parseInt(ary[1]));
+                    count++;
+                }
+                else{
+                    if(ary[2] == "HORIZONTAL"){
+                        ships.add(new Ship(board, Integer.parseInt(ary[0]), Integer.parseInt(ary[1]), Ship.Orientation.HORIZONTAL, Integer.parseInt(ary[3])));
+                    }
+                    else {
+                        ships.add(new Ship(board, Integer.parseInt(ary[0]), Integer.parseInt(ary[1]), Ship.Orientation.VERTICAL,Integer.parseInt(ary[3])));
+                    }
+                }
+            }
+        }
+        catch (IOException bad){
+            System.out.println("The file is invalid");
+        }
+    }
+
+    /*
+    this hits a cell
+     */
+    private void hit(String[]args){
+        try{
+            Cell cell = board.getCell(Integer.parseInt(args[1]), Integer.parseInt(args[2]));
+            cell.hit();
+        }
+        catch (BattleshipException thing){
+            System.out.println(OutOfBoundsException.PAST_EDGE);
+        }
+    }
+
+    /*
+    returns boolean stating status of game true == game over
+     */
+    private boolean quit() {
+        if (board.allSunk() == true) {
+            return true;
+        }
+        return false;
+    }
+
+    private void play(){
+
+    }
+
+
+
 }
